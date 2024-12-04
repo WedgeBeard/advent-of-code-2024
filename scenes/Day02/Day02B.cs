601a0fc46b343279b66e799078005d4eaeb3d3ad
WedgeBeard <tysonstecklein@gmail.com>
2024-12-03 23:19:08 -0700

    finished day 2 part 2

--- scenes/Day02/Day02B.cs
+++ scenes/Day02/Day02B.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using Godot;
+using Microsoft.Win32.SafeHandles;
 
 namespace Day02;
 
@@ -14,20 +15,59 @@ public class Day02B {
 
         foreach(string line in results) {
             string[] sequence = line.Split(' ');
-            if (CheckSafety(sequence)) { 
-                safeTotal++;
-            }
-            else {
-                bool anySafe = false;
-                for (int i = 0; i < sequence.Length - 1; i++) {
-                    string[] testSequence = RemoveElement(sequence, i);
-                    if(CheckSafety(testSequence)) { anySafe = true; }
-                }
-                if(anySafe) { safeTotal++; }
-            }
+            if(IsAscending(sequence) && IsSafe(sequence)) { safeTotal++; }
+            else if (IsDescending(sequence) && IsSafe(sequence)) { safeTotal++; }
+            else if (TryRemoval(sequence)) { safeTotal++; }
+        }
+        GD.Print("Grand total Day 2 part 2: " + safeTotal);
+    }
+
+    public bool IsAscending(string[] sequence) {
+        bool ascending = true;
+
+        for(int i = 0; i < sequence.Length - 1; i++) {
+            int curr = int.Parse(sequence[i]);
+            int next = int.Parse(sequence[i+1]);
+            if (curr > next) { ascending = false; }
         }
 
-        GD.Print("Grand total Day 2 part 2: " + safeTotal);
+        return ascending;
+    }
+
+    public bool IsDescending(string[] sequence) {
+        bool descending = true;
+
+        for(int i = 0; i < sequence.Length - 1; i++) {
+            int curr = int.Parse(sequence[i]);
+            int next = int.Parse(sequence[i+1]);
+            if (curr < next) { descending = false; }
+        }
+
+        return descending;
+    }
+
+    public bool IsSafe(string[] sequence) {
+        bool safe = true;
+
+         for(int i = 0; i < sequence.Length - 1; i++) {
+            int curr = int.Parse(sequence[i]);
+            int next = int.Parse(sequence[i+1]);
+            if (Math.Abs(curr - next) < 1 || Math.Abs(curr - next) > 3) { safe = false; }
+        }
+
+        return safe;
+    }
+
+    private bool TryRemoval(string[] sequence) {
+        bool safe = false;
+
+        for(int i = 0; i < sequence.Length; i++) {
+            string[] testSequence = RemoveElement(sequence, i);
+            if(IsAscending(testSequence) && IsSafe(testSequence)) { safe = true; }
+            else if (IsDescending(testSequence) && IsSafe(testSequence)) { safe = true; }
+        }
+
+        return safe;
     }
 
     private string[] RemoveElement(string[] str, int index)
@@ -42,28 +82,5 @@ public class Day02B {
         }
 
         return newSequence;
-    }
-
-
-    private bool CheckSafety(string[] sequence) {
-        bool safe = false;
-         int safeCount = 0;
-            int sequenceLength = sequence.Length;
-            for(int i = 0; i < sequenceLength - 1; i++) {
-                int curr = int.Parse(sequence[i]);
-                int next = int.Parse(sequence[i+1]);
-                if (curr < next && next - curr >= 1 && next - curr <= 3) {
-                    safeCount++;
-                } else if (curr > next && curr - next >= 1 && curr - next <= 3) {
-                    safeCount--;
-                } else {
-                    break;
-                }
-            }
-            if(Math.Abs(safeCount) == sequenceLength-1) {
-                safe = true;
-            }
-
-        return safe;
     }
 }
