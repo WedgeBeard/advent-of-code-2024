4c6eec7a0de2e5c4f0d55f032adb1acf5b87af39
WedgeBeard <tysonstecklein@gmail.com>
2024-12-03 23:52:26 -0700

    finished day 3 part 1

--- scenes/Day03/Day03A.cs
+++ scenes/Day03/Day03A.cs
@@ -1,11 +1,32 @@
 
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
 using Godot;
 
 namespace Day03;
 
 public class Day03A {
     public void ComputeDay03A(string directory) {
+        string filePath = Path.Combine(directory, "scenes\\Day03\\day03a.txt");
+        List<string> results = utils.FileReader.ToStringList(filePath);
 
-        GD.Print("Grand total Day 3 part 1: ");
+        string mulpattern = @"mul\(\d{1,3},\d{1,3}\)";
+        Regex mulRegex = new Regex(mulpattern);
+
+        string numPattern = @"\d{1,3}";
+        Regex numRegex = new Regex(numPattern);
+
+        int total = 0;
+
+        for(int i = 0; i < results.Count; i++) {
+            MatchCollection mulMatches = mulRegex.Matches(results[i]);
+            foreach(Match mulMatch in mulMatches) {
+                MatchCollection numMatches = numRegex.Matches(mulMatch.Value);
+                total += int.Parse(numMatches[0].Value) * int.Parse(numMatches[1].Value);
+            }
+        }
+
+        GD.Print("Grand total Day 3 part 1: " + total);
     }
 }
