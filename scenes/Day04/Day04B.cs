09dc4ab456f758e2e7ebbc6f699c11df78ca1c6d
WedgeBeard <tysonstecklein@gmail.com>
2024-12-08 22:30:02 -0700

    starting day 4 part 2

--- scenes/Day04/Day04B.cs
+++ scenes/Day04/Day04B.cs
@@ -1,11 +1,17 @@
-using Godot;
-using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Day04;
 
 public class Day04B
 {
 	public void ComputeDay04B(string directory) {
-		
+		string filePath = Path.Combine(directory, "scenes\\Day04\\day04a.txt");
+        List<string> fileStrings = utils.FileReader.ToStringList(filePath);
+
+		int maxRows = fileStrings.Count;
+		int maxCols = fileStrings[0].Length;
+		char match = 'A';
+
 	}
 }
