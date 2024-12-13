9c904f02b4628fd1d890e1d8db298d2b19d1e875
WedgeBeard <tysonstecklein@gmail.com>
2024-12-13 16:41:09 -0700

    starting to do day 7

--- scenes/Day07/Day07A.cs
+++ scenes/Day07/Day07A.cs
@@ -1,10 +1,49 @@
 
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
 using Godot;
 
 namespace Day07;
 
 public class Day07A {
     public void ComputeDay07A(string directory) {
-        GD.Print("Day 07 Part 1: ");
+        string filePath = Path.Combine(directory, "scenes\\Day07\\day07atest.txt");
+        List<string> fileStrings = utils.FileReader.ToStringList(filePath);
+        
+        string totalPattern = @"(\d+)(?=:)";
+        string factorsPattern = @"(?<=:\s|\G\s)\d+";
+        Regex totalRegex = new Regex(totalPattern);
+        Regex factorsRegex = new Regex(factorsPattern);
+
+        for (int i = 0; i < fileStrings.Count; i++) {
+            string line = fileStrings[i];
+            long total = long.Parse(totalRegex.Match(line).Value);
+            List<long> factors = new List<long>();
+            MatchCollection factorsMatches = factorsRegex.Matches(line);
+            
+            foreach(Match match in factorsMatches) { factors.Add(long.Parse(match.Value)); }
+
+            List<string> operators = new List<string>();
+            GenerateOperators("", factors.Count - 1, operators);
+
+            long sum = 0;
+            foreach(long factor in factors) {
+                // do all the math stuff
+            }
+        }
+
+        GD.Print("Day 07 Part 2: ");
+    }
+
+    private void GenerateOperators(string prefix, int length, List<string> permutations)
+    {
+        if (length == 0) {
+            permutations.Add(prefix); 
+            return; 
+        }
+        
+        GenerateOperators(prefix + "+", length - 1, permutations);
+        GenerateOperators(prefix + "*", length - 1, permutations);
     }
 }
--- scenes/Day07/Day07B.cs
+++ scenes/Day07/Day07B.cs
@@ -1,10 +1,33 @@
 
+using System;
+using System.Collections.Generic;
+using System.IO;
 using Godot;
 
 namespace Day07;
 
 public class Day07B {
     public void ComputeDay07B(string directory) {
+        string filePath = Path.Combine(directory, "scenes\\Day07\\day07atest.txt");
+        List<string> fileStrings = utils.FileReader.ToStringList(filePath);
+        
+        List<string> permutations = new List<string>(); 
+        GeneratePermutations("", 6, permutations); 
+        GD.Print("All permutations of length 6:"); 
+        foreach (var permutation in permutations) { 
+            GD.Print(permutation); 
+        }
         GD.Print("Day 07 Part 2: ");
+    }
+
+    private void GeneratePermutations(string prefix, int length, List<string> permutations)
+    {
+        if (length == 0) { 
+            permutations.Add(prefix); 
+            return; 
+        } 
+        
+        GeneratePermutations(prefix + "+", length - 1, permutations); 
+        GeneratePermutations(prefix + "*", length - 1, permutations);
     }
 }
